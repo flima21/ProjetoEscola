857fa4dec91864d1905d6a5bb08aa06482a0b9bf
Felipe Pedroso de Lima <felipe.pedroso2108@gmail.com>
2024-03-13 20:31:36 -0300

    13-03 Remoção de pacotes que não usados

--- src/main/java/com/example/projetoescola/repository/CursoRepository.java
+++ src/main/java/com/example/projetoescola/repository/CursoRepository.java
@@ -5,12 +5,9 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
-import com.example.projetoescola.models.CategoriaCurso;
 import com.example.projetoescola.models.Curso;
 
 import jakarta.persistence.EntityManager;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
 import jakarta.persistence.TypedQuery;
 import jakarta.transaction.Transactional;
 
